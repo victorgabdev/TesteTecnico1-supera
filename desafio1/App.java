2b559685de70538891e6ad1cf93dfbb211454a54
Victor Gabriel <victorgabdev@gmail.com>
2023-07-14 18:08:57 -0300

    Challenge one answered

--- desafio1/App.java
+++ desafio1/App.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class App {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        int numbersQuantity = scan.nextInt();
+        List<Integer> pairNumbers = new ArrayList<>();
+        List<Integer> oddNumbers = new ArrayList<>();
+
+        for (int index = 0; index < numbersQuantity; index++) {
+            int number = scan.nextInt();
+            if (number % 2 == 0) {
+                pairNumbers.add(number);
+            } else {
+                oddNumbers.add(number);
+            }
+        }
+
+        List<Integer> pairNumbersCrescent = pairNumbers.stream().sorted().toList();
+        List<Integer> oddNumberDecrescent = oddNumbers.stream().sorted(Comparator.reverseOrder()).toList();
+
+        System.out.println("--------------------");
+        pairNumbersCrescent.forEach(System.out::println);
+        oddNumberDecrescent.forEach(System.out::println);
     }
 }
