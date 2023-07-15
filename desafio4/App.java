d02f1682e956d34913d768e365fdad62d7995874
Victor Gabriel <victorgabdev@gmail.com>
2023-07-15 12:16:43 -0300

    Updated solution for challenge 4

--- desafio4/App.java
+++ desafio4/App.java
@@ -6,49 +6,49 @@ public class App {
     public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Digite a quantidade de frases infectadas: ");
-            int quantidadeFrases = scanner.nextInt();
+            System.out.print("Quantidade de frases infectadas: ");
+            int phrasesQuantity = scanner.nextInt();
             scanner.nextLine(); // Consumir a quebra de linha após a leitura do inteiro
 
-            List<String> frasesInfectadas = new ArrayList<>();
+            List<String> infectedPhrases = new ArrayList<>();
 
             System.out.println("Digite as frases infectadas:");
 
-            for (int i = 0; i < quantidadeFrases; i++) {
-                String fraseInfectada = scanner.nextLine();
-                frasesInfectadas.add(fraseInfectada);
+            for (int index = 0; index < phrasesQuantity; index++) {
+                String infectedPhrase = scanner.nextLine();
+                infectedPhrases.add(infectedPhrase);
             }
 
-            List<String> frasesDecifradas = decifrarFrases(frasesInfectadas);
+            List<String> decipheredPhrases = decipherPhrases(infectedPhrases);
 
-            System.out.println("Frases decifradas:");
+            System.out.println("----------------------");
 
-            for (String fraseDecifrada : frasesDecifradas) {
-                System.out.println(fraseDecifrada);
+            for (String decipheredPhrase : decipheredPhrases) {
+                System.out.println(decipheredPhrase);
             }
         }
 
-        public static List<String> decifrarFrases (List < String > frasesInfectadas) {
-            List<String> frasesDecifradas = new ArrayList<>();
+        public static List<String> decipherPhrases(List < String > infectedPhrases) {
+            List<String> decipheredPhrases = new ArrayList<>();
 
-            for (String fraseInfectada : frasesInfectadas) {
-                String decifrada = decifrarLinha(fraseInfectada);
-                frasesDecifradas.add(decifrada);
+            for (String infectedPhrase : infectedPhrases) {
+                String deciphered = decipherLine(infectedPhrase);  // aqui
+                decipheredPhrases.add(deciphered);
             }
 
-            return frasesDecifradas;
+            return decipheredPhrases;
         }
 
-        public static String decifrarLinha (String linha){
-            int tamanho = linha.length();
-            StringBuilder sb = new StringBuilder(tamanho);
+        public static String decipherLine(String line){
+            int size = line.length();
+            StringBuilder sb = new StringBuilder(size);
 
-            for (int i = tamanho / 2 - 1; i >= 0; i--) {
-                sb.append(linha.charAt(i));
+            for (int index = size / 2 - 1; index >= 0; index--) {
+                sb.append(line.charAt(index));
             }
 
-            for (int i = tamanho - 1; i >= tamanho / 2; i--) {
-                sb.append(linha.charAt(i));
+            for (int index = size - 1; index >= size / 2; index--) {
+                sb.append(line.charAt(index));
             }
 
             return sb.toString();
